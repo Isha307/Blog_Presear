76e0bf928051cc1e6e28e7d40d36af89db96c24a
Isha307 <56998032+Isha307@users.noreply.github.com>
2022-05-24 19:30:00 +0530

    changes in model

--- blogx/admin.py
+++ blogx/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Profile, Post, Tag, Category, Comment, reply
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(reply)
--- blogx/models.py
+++ blogx/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from froala_editor.fields import FroalaField
 
+
 class Profile(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE)
     image = models.ImageField(upload_to='profile_pic')
@@ -12,6 +13,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    bookmark = models.ForeignKey('Bookmark', on_delete=models.CASCADE, blank=True)
 
 
 class Post(models.Model):
@@ -22,20 +24,58 @@ class Post(models.Model):
     image = models.ImageField(upload_to='blog')
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = models.ManyToManyField('Tag', blank=True)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
+    subcategory = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
     is_draft = models.BooleanField(default=False)
     likes = models.ManyToManyField(User, blank=True, related_name='likes')
+
+class Video(models.Model):
+    title = models.CharField(max_length=256, default='new blog')
+    content = FroalaField()
+    series = models.IntegerField()
+    pub_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='blog')
+    video = models.FileField(upload_to='video')
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag', blank=True)
+    subcategory = models.ForeignKey('subcategory', on_delete=models.CASCADE, null=True, blank=True)
+
+    
+class Story(models.Model):
+    subcategory = models.ForeignKey('subcategory', on_delete=models.CASCADE, null=True, blank=True)
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    logo = models.ImageField(upload_to='logo')
+    description = models.TextField()
+    headquartes = models.TextField()
+    business_model = models.CharField(max_length=100)
+    founding_date = models.DateField()
+    employees_number = models.IntegerField()
+    CoreTeam = models.TextField()
+    facebook = models.TextField(blank=True)
+    twitter = models.TextField(blank=True)
+    linkedin = models.TextField(blank=True)
+
+class CompanyPost(models.Model):
+    post = models.ManyToManyField('Post', blank=True)
+    company_name = models.ForeignKey('Company', on_delete=models.CASCADE, null=True, blank=True)
 
 class Tag(models.Model):
     name = models.CharField(max_length=256)
 
-class Category(models.Model):
+class subcategory(models.Model):
     cat_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     url = models.CharField(max_length=100)
     image = models.ImageField(upload_to='category')
     add_date = models.DateTimeField(auto_now_add=True, null=True)
+
+class Category(models.Model):
+    story = models.ForeignKey('Story', on_delete=models.CASCADE)
+    video = models.ForeignKey('Video', on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -48,5 +88,10 @@ class reply(models.Model):
 	reply = models.ForeignKey('Comment', on_delete=models.CASCADE, related_name="replies", null=True)
 	rep = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    post = models.ManyToManyField('Post', blank=True)
+
     
 
