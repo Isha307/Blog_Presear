f381939cc81825c403638617855c09c3533e21eb
Isha307 <56998032+Isha307@users.noreply.github.com>
2022-06-02 09:24:18 +0530

    Changes

--- blogx/serializers.py
+++ blogx/serializers.py
@@ -3,7 +3,7 @@ from .models import Profile, Post, Tag, Category, Comment, Reply, User
 from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-#from .utils import Util
+from .utils import Util
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
   password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
