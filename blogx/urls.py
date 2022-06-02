0e3a312c78f2e0345290034b58ce38a9616b380c
Neelam Chanapati <67052884+NeelamChanapati@users.noreply.github.com>
2022-06-02 09:27:16 +0530

    urls updated

--- blogx/urls.py
+++ blogx/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path,include
 from blogx.views import UserRegistrationView, UserLoginView, UserChangePasswordView, SendPasswordResetEmailView, UserPasswordResetView
 from rest_framework.authtoken.views import obtain_auth_token
-
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('changepassword/', UserChangePasswordView.as_view(), name='changepassword'),
     path('passwordreset/', SendPasswordResetEmailView.as_view(), name='passwordreset'),
-     path('resetpassword/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('resetpassword/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
+    path('posts', PostList.as_view()),
+    path('postdetail/<int:pk>', PostDetail.as_view()),
 
 ]
